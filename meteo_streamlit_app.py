729e34aa78a3febf3d2aed20ae87b2b1ccc7600c
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-04-19 20:29:16 +0200

    Add files via upload

--- meteo_streamlit_app.py
+++ meteo_streamlit_app.py
@@ -1347,7 +1347,7 @@ elif mode == "Mapy Aladin":
     st.markdown("### Víc map z Aladina najdete tady:")
     st.markdown("[https://aladin-open-data-chmu.streamlit.app/](https://aladin-open-data-chmu.streamlit.app/)")
 
-    st.markdown("###  ")
+    st.markdown("#####  ")
     st.markdown(
         '##### 24h srážky ▼',
         unsafe_allow_html=True
