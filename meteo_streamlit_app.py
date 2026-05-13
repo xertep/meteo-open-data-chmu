377023b6251cc0c68cd85aa6c612835edf2e150d
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-05-13 18:11:58 +0200

    Update meteo_streamlit_app.py

--- meteo_streamlit_app.py
+++ meteo_streamlit_app.py
@@ -1338,7 +1338,7 @@ def get_warnings_for_region(region_name):
 
     return region_warnings
 
-@st.cache_data(ttl=120, show_spinner="Načítám data...")
+@st.cache_data(ttl=120, show_spinner="Hledám případné výstrahy...")
 def fetch_warnings_html(region_code):
     if region_code == "CR":
         return ""  #  no warnings for whole country
